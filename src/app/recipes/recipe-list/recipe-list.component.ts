f26ab71a28b7d6737a3e5d641cc81e3a1ddd8333
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-10 19:10:44 +0200

    added template to RecipeComponent

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe";
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: 'recipe-list.component.html',
-  styleUrls: ['recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
+  recipe = new Recipe('Dress', 'Nice black dress', 'https://s-media-cache-ak0.pinimg.com/736x/ac/ba/d9/acbad9b0d9aeb233da529f5444b03281.jpg');
 
   constructor() { }
 
