e2b5fd581bab215d2a217067fcc4ece8fbad33fa
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-10 20:36:03 +0200

    passed recipe-data from recipeList component to recipe-detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import {Recipe} from "../recipe";
+import {Output} from "@angular/core/src/metadata/directives";
 
 @Component({
   selector: 'rb-recipe-list',
@@ -8,10 +9,14 @@ import {Recipe} from "../recipe";
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
   recipe = new Recipe('Dress', 'Nice black dress', 'https://s-media-cache-ak0.pinimg.com/736x/ac/ba/d9/acbad9b0d9aeb233da529f5444b03281.jpg');
-
+  @Output() recipeSelectedItem = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectRecipe(recipe: Recipe){
+    this.recipeSelectedItem.emit(recipe)
   }
 
 }
