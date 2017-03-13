79892e10729c9d5b102d909d3bc0b4ade3be8b2f
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-13 20:14:37 +0200

    create dropdown Directive add it in header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.comp
 
 //services
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
+import { DropdownDirective } from './dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import {ShoppingListService} from "./shopping-list/shopping-list.service";
     RecipeItemComponent,
     RecipeDetailComponent,
     ShoppingListComponent,
-    ShoppingListAddComponent
+    ShoppingListAddComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'rb-header',
   templateUrl: './header.component.html'
