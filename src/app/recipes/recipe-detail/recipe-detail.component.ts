3f6982d81df21f5fd8f6bca7439128f50319b69b
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-16 15:25:32 +0200

    added method for unsubscribe in recipe-detail component

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, OnDestroy} from '@angular/core';
 import { Recipe } from "../recipe";
 import {ShoppingListService} from "../../shopping-list/shopping-list.service";
 import {Router, ActivatedRoute} from "@angular/router";
 import {RecipeService} from "../recipe.service";
+import {Subscription} from "rxjs/Rx";
 
 @Component({
   selector: 'rb-recipe-detail',
   templateUrl: './recipe-detail.component.html'
 })
-export class RecipeDetailComponent implements OnInit {
+export class RecipeDetailComponent implements OnInit, OnDestroy{
   selectedRecipe: Recipe;
 
-  recipeId: number;
+  private recipeId: number;
+  private subscription: Subscription;
 
   constructor(private sls: ShoppingListService,
               private router: Router,
@@ -19,12 +21,16 @@ export class RecipeDetailComponent implements OnInit {
               private  route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.params.subscribe(
+    this.subscription = this.route.params.subscribe(
       (param: any) => {
         this.recipeId = param['id'];
         // console.log(this.recipeId);
         this.selectedRecipe = this.recipeService.getRecire(this.recipeId)
       });
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe()
   }
 
   onAddToList(){
