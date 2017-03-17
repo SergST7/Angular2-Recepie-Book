dc49e4ee0f4dac3710ff274d33f0f689187bd25c
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-17 18:39:45 +0200

    added disable class at button, added func in shoppinglist service addItem, added func onSubmit in shopping list add component

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from "../shared/Ingredient";
+import {ShoppingListService} from "./shopping-list.service";
 
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
 export class ShoppingListAddComponent implements OnInit {
+  item: Ingredient;
+  isAdd = true;
 
-  isAdd = true
-
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onSubmit(){
+  onSubmit(val: Ingredient){
+    if (this.isAdd){
+      this.item = new Ingredient(val.name, val.amount);
+      this.sls.addItem(this.item)
+    } else {
+      //edit
+    }
 
   }
 
